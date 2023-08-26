b32779faafc084384e6f2e27d4cfd02a5032b5c8
sridama456 <143206786+sridama456@users.noreply.github.com>
2023-08-26 21:29:11 +0530

    Add files via upload

---
+++ calcu/script.js
@@ -0,0 +1,22 @@
+let displayValue = "";
+
+function appendToDisplay(value) {
+  displayValue += value;
+  document.getElementById("result").value = displayValue;
+}
+
+function clearDisplay() {
+  displayValue = "";
+  document.getElementById("result").value = displayValue;
+}
+
+function calculate() {
+  try {
+    displayValue = eval(displayValue);
+    document.getElementById("result").value = displayValue;
+  } catch (error) {
+    document.getElementById("result").value = "Error";
+  }
+}
+
+// Rest of your JavaScript code (button generation, event listeners, etc.)
